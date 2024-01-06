ebc3c337d1e7a47a66595aa69e6da5c7034af7da
kondetiharsha <119839744+kondetiharsha@users.noreply.github.com>
2024-01-06 11:58:39 +0530

    import java.util.*; public class MyClass{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         float a=sc.nextFloat();         float b=sc.nextFloat();         float c=a*b;         System.out.printf("%.2f",c);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MyClass{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextFloat();
+        float b=sc.nextFloat();
+        float c=a*b;
+        System.out.printf("%.2f",c); 
+    }
+}
