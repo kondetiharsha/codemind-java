142f5499af19f9f1846fa354218a668c105a57fa
kondetiharsha <119839744+kondetiharsha@users.noreply.github.com>
2024-02-05 11:19:04 +0530

    import java.util.*; public class M{     public static void main(String[] aargs){         Scanner sc=new Scanner(System.in);         double c=sc.nextInt();         double f=32+(c*9/5);         System.out.printf("%.2f",f);
        }
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class M{
+    public static void main(String[] aargs){
+        Scanner sc=new Scanner(System.in);
+        double c=sc.nextInt();
+        double f=32+(c*9/5);
+        System.out.printf("%.2f",f);
+       
+        
+    }
+}
