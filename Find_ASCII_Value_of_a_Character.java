29af86bbcd0b5e344a81f2e9d140cf6648fc8037
kondetiharsha <119839744+kondetiharsha@users.noreply.github.com>
2024-02-05 10:31:22 +0530

    import java.util.*; public class M{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         char s=sc.next().charAt(0);         int f=(int)s;         System.out.print(f);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class M{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        char s=sc.next().charAt(0);
+        int f=(int)s;
+        System.out.print(f);
+    }
+}
