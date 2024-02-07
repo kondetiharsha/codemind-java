d571ece312c7855c898038a446589479fea83261
kondetiharsha <119839744+kondetiharsha@users.noreply.github.com>
2024-02-07 11:09:12 +0530

    import java.util.*; public class M{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         if(a>40){             System.out.print("YES");
            }
            else{
                System.out.print("NO");
            }
        }
    }

---
+++ Subscription.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class M{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>40){
+            System.out.print("YES");
+        
+        }
+        else{
+            System.out.print("NO");
+        }
+    }
+}
